69666ecf1ec026d39093554eed6fc146ec28800d
blakelazarus7 <blake@eatfare.com>
2025-07-02 13:09:40 -0700

    Update trace.js

--- api/trace.js
+++ api/trace.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const sku = req.query.sku;
-  const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
+const AIRTABLE_API_KEY = "patX9RAJJXpjbOq05.9a2ae2b9e396d5abfb7fe8e894e55321abbcb30db9d77932bff5b0418c41f21a";
 
   if (!sku) {
     return res.status(400).json({ error: "Missing SKU in query." });
